b312ded11f4d6d2ea94676d2607801e9758d101b
hemant <hybhoyar@gmail.com>
2016-06-11 22:31:33 +0530

    moved th service

--- rest_fuzzy/fuzzybot/views.py
+++ rest_fuzzy/fuzzybot/views.py
@@ -1,38 +1,19 @@
-from chatterbot.training.trainers import ListTrainer
 from django.views.generic import View
 from django.http import JsonResponse
-from chatterbot import ChatBot
-from rest_framework.views import APIView
 from rest_framework.renderers import JSONRenderer
 import logging
+import unicodedata as ud
 
+from services import getBot
 
-chatterbot = ChatBot(
-    'Example ChatterBot',
-    io_adapter="chatterbot.adapters.io.JsonAdapter"
-)
-
-chatterbot.set_trainer(ListTrainer)
-chatterbot.train([
-    "Hi",
-    "Hello",
-    "How are you?",
-    "I am good.",
-    "That is good to hear.",
-    "Thank you",
-    "You are welcome.",
-])
-
+chatterbot = getBot()
 logger = logging.getLogger(__name__)
 class ChatterBotView(View):
 
+    def get(self, request, *args, **kwargs):
+        return JsonResponse("hello", safe=False)
+
     def post(self, request, *args, **kwargs):
-
-        logger.info("entered the method post")
-
         input_statement = request.POST.get('text')
-
-        response_data = chatterbot.get_response(input_statement)
-
-        logger.error(response_data)
+        response_data = chatterbot.get_response(ud.normalize('NFKD', input_statement).encode('ascii','ignore'))
         return JsonResponse(JSONRenderer().render(response_data.text),safe=False)
