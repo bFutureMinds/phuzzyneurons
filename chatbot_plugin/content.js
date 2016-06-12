cbb5e5d8f9b4b5aed887901dd0a13a34283e375a
ankitm2210 <ankit.mishra100@gmail.com>
2016-06-12 11:16:05 +0530

    "Pointing the plugin to locol host web app"

--- chatbot_plugin/content.js
+++ chatbot_plugin/content.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
     	console.dir($);
-      $('p.text-text-muted').html("<a href=\"chatterbot.org\">Click Here To Open ChatBot</a>");
+      $('p.text-text-muted').html("<a href=\"http://localhost:8080/\">Click Here To Open ChatBot</a>");
     }
   }
 );
